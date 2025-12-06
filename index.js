f596fe68faf0e93d6d5ae48b2e43d13e1fc149a9
amritgrg2033 <amrit222457@gmail.com>
2025-12-06 11:53:31 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// Array of banner images
+const bannerImages = [
+    "/image/banner-3.avif",
+    "/image/banner2.png",
+    
+];
+
+let index = 0;
+const bannerPhoto = document.getElementById("banner-pho");
+
+// Change image every 3 seconds
+setInterval(() => {
+    index = (index + 1) % bannerImages.length;  // infinite loop
+    bannerPhoto.src = bannerImages[index];
+}, 3000); // change every 3s
